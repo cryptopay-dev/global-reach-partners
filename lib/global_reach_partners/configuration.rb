e32be033c4644734d327a70c5f68382970046edd
Aleksey Demidov <aleksey.dem@gmail.com>
2017-09-07 14:31:19 +0300

    [DEVX-909] Add optional fxplugin and tradeservice wsdls

--- lib/global_reach_partners/configuration.rb
+++ lib/global_reach_partners/configuration.rb
@@ -1,9 +1,16 @@
 module GlobalReachPartners
   class Configuration
-    attr_accessor :client_code, :username, :password, :url, :proxy
+    attr_accessor :client_code
+    attr_accessor :username
+    attr_accessor :password
+    attr_accessor :proxy
+    attr_accessor :url
+    attr_accessor :trade_service_wsdl
+    attr_accessor :fx_plugin_wsdl
 
     # Default values
-    attr_accessor :debug, :soap_version
+    attr_accessor :debug
+    attr_accessor :soap_version
 
     def initialize(**attrs)
       attrs.each do |key, value|
