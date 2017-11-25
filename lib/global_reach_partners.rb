bd853c5d9983e2512a072fb3768f28fbb1f6345c
Aleksey Demidov <aleksey.dem@gmail.com>
2017-11-25 12:27:41 +0300

    [DEVX-000] Trade by workdays

--- lib/global_reach_partners.rb
+++ lib/global_reach_partners.rb
@@ -46,6 +46,16 @@ module GlobalReachPartners
       @trade_service_client ||= client(trade_service_wsdl)
     end
 
+    def closest_workday(date = Date.today)
+      if date.saturday?
+        date + 2.days
+      elsif date.sunday?
+        date + 1.day
+      else
+        date
+      end
+    end
+
     private
 
     def fx_plugin_wsdl
---
+++ spec/global_reach_partners_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+RSpec.describe GlobalReachPartners do
+  subject(:grp) { described_class }
+
+  describe '.closest_workday' do
+    it 'returns GRP workday' do
+      aggregate_failures do
+        # 01/05/2017 - Monday
+        expect(grp.closest_workday(Date.parse('01/05/2017'))).to eq Date.parse('01/05/2017')
+        expect(grp.closest_workday(Date.parse('02/05/2017'))).to eq Date.parse('02/05/2017')
+        expect(grp.closest_workday(Date.parse('03/05/2017'))).to eq Date.parse('03/05/2017')
+        expect(grp.closest_workday(Date.parse('04/05/2017'))).to eq Date.parse('04/05/2017')
+        expect(grp.closest_workday(Date.parse('05/05/2017'))).to eq Date.parse('05/05/2017')
+        expect(grp.closest_workday(Date.parse('06/05/2017'))).to eq Date.parse('08/05/2017')
+        expect(grp.closest_workday(Date.parse('07/05/2017'))).to eq Date.parse('08/05/2017')
+        expect(grp.closest_workday(Date.parse('08/05/2017'))).to eq Date.parse('08/05/2017')
+      end
+    end
+  end
+end
