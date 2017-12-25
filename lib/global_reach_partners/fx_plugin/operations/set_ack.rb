09f716c8c3fcc31253d1c568ba5245f1890a9847
Aleksey Demidov <aleksey.dem@gmail.com>
2017-12-25 14:46:45 +0300

    [DEVX-1797] Add logs for get_rate_matrix and set_ack

--- lib/global_reach_partners/fx_plugin/operations/set_ack.rb
+++ lib/global_reach_partners/fx_plugin/operations/set_ack.rb
@@ -4,13 +4,20 @@ module GlobalReachPartners
       module SetAck
         extend self
 
+        delegate :logger, to: GlobalReachPartners
+
         def call(guid)
+          logger.info("GlobalReachPartners.set_ack start: #{guid}")
+
           message = {
             error_msg: '',
             guid: guid
           }
 
           response = Request.new(:set_ack).call(message)
+
+          logger.info("GlobalReachPartners.set_ack succeed: #{response}")
+
           response.body.dig(:set_ack_response, :error_msg) == 'ForValidation'
         end
       end
